aa90fe867f54f7d2da12cbc4ee9c686824356729
subhaskushwaha <subhaskushwahak@gmail.com>
2026-04-27 11:45:08 +0530

    update env file

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-  origin: "http://localhost:3000",
+  origin: "https://callcenterdataalloments.netlify.app",
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true
 }));
