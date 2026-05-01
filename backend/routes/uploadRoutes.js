84bb71ef9b73bae79ba6b5fd85b141e5396d6222
Subhas kushwaha <167758912+subhaskushwaha@users.noreply.github.com>
2026-05-01 16:22:11 +0530

    Delete backend directory

--- backend/routes/uploadRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-const router = express.Router();
-
-import { uploadLeads, getUploads } from "../controllers/uploadController.js";
-
-router.post("/upload", uploadLeads);
-router.get("/uploads", getUploads);
-
-export default router;
